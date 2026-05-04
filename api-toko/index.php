cec370c0a18b0d845257a2f9c1d4db2f16cdc417
Ridwan-TI <ridwankece2211@gmail.com>
2026-05-04 11:21:07 +0700

    Update index.php

--- api-toko/index.php
+++ api-toko/index.php
@@ -1,10 +1,18 @@
 <?php
-header("Content-Type: application/json");
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once __DIR__ . '/koneksi.php';
 
 $query = mysqli_query($koneksi, "SELECT * FROM barang");
-$data = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
-echo json_encode($data);
+if (!$query) {
+    die("Query gagal: " . mysqli_error($koneksi));
+}
 
+while ($data = mysqli_fetch_array($query)) {
+    echo "ID: " . $data['ID'] . "<br>";
+    echo "Nama Barang: " . $data['nama_barang'] . "<br>";
+    echo "Harga: " . $data['harga'] . "<br><br>";
+}
 ?>
